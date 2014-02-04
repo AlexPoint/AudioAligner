8cb775e9ec19c62a89fd802ca5cf43c9708a2207
Alexandre Point <apoint@fleex.tv>
2014-02-04 17:56:43 +0100

    New: added code to download youtube audio file

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using AudioAligner.Classes.AudioAligner;
+using YoutubeExtractor;
 
 namespace Test
 {
@@ -14,7 +15,7 @@ namespace Test
         static void Main(string[] args)
         {
             var pathToProject = Environment.CurrentDirectory + "/../..";
-            var pathToAudioFile = pathToProject + "/resource/wav/dedication.wav";
+            /*var pathToAudioFile = pathToProject + "/resource/wav/dedication.wav";
             var pathToTranscriptFile = pathToProject + "/resource/transcription/dedication.txt";
             Aligner aligner = new Aligner("../../resource/config.xml",	pathToAudioFile, pathToTranscriptFile);	
 		    //Aligner aligner = new Aligner("./src/config.xml",	relativePathToAudio, relativePathToTranscript);
@@ -28,9 +29,49 @@ namespace Test
 		    aligner.setBackwardJumpProbability(0.001);
 		    //BufferedReader reader = new BufferedReader(new FileReader("./result.txt"));
 		    string result = aligner.align();
-		    Console.WriteLine(result);
+		    Console.WriteLine(result);*/
 
+
+            var downloadFolderPath = pathToProject + "/downloads";
+            // Louis CK video
+            var youtubeId = "Y8ynUspj4c8";
+            var youtubeLink = GetYoutubeLink(youtubeId);
+
+            IEnumerable<VideoInfo> videoInfos = DownloadUrlResolver.GetDownloadUrls(youtubeLink);
+
+            /*
+            * We want the first extractable video with the highest audio quality.
+            */
+            VideoInfo video = videoInfos
+                .Where(info => info.CanExtractAudio)
+                .OrderByDescending(info => info.AudioBitrate)
+                .First();
+
+            /*
+             * Create the audio downloader.
+             * The first argument is the video where the audio should be extracted from.
+             * The second argument is the path to save the audio file.
+             */
+            var audioDownloader = new AudioDownloader(video, Path.Combine(downloadFolderPath, video.Title + video.AudioExtension));
+
+            // Register the progress events. We treat the download progress as 85% of the progress and the extraction progress only as 15% of the progress,
+            // because the download will take much longer than the audio extraction.
+            audioDownloader.DownloadProgressChanged += (sender, arguments) => Console.WriteLine(arguments.ProgressPercentage * 0.85);
+            audioDownloader.AudioExtractionProgressChanged += (sender, arguments) => Console.WriteLine(85 + arguments.ProgressPercentage * 0.15);
+
+            /*
+             * Execute the audio downloader.
+             * For GUI applications note, that this method runs synchronously.
+             */
+            audioDownloader.Execute();
+
+            Console.WriteLine("Download ok");
             Console.ReadLine();
+        }
+
+        private static string GetYoutubeLink(string youtubeId)
+        {
+            return string.Format("http://www.youtube.com/watch?v={0}", youtubeId);
         }
     }
 }
